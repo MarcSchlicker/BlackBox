cca6a1a3b8f152fbc54304b078be253ce04a8899
MarcSchlicker <marcschlicker@gmail.com>
2025-02-12 01:29:45 +0100

    +Opferschwert

---
+++ src/main/java/net/mcreator/blackbox/init/BlackboxModItems.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.blackbox.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import net.minecraft.world.item.Item;
+
+import net.mcreator.blackbox.item.OpferschwertItem;
+import net.mcreator.blackbox.BlackboxMod;
+
+public class BlackboxModItems {
+	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(BlackboxMod.MODID);
+	public static final DeferredItem<Item> OPFERSCHWERT = REGISTRY.register("opferschwert", OpferschwertItem::new);
+	// Start of user code block custom items
+	// End of user code block custom items
+}
